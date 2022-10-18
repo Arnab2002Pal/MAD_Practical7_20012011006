2af74a2f1013e94c0322c6cc5122eb2825e14b6d
ARNAB <https://github.com/Arnab2002Pal/MAD_Practical3_20012011006>
2022-10-18 12:00:43 +0530

    Done!

---
+++ app/src/main/java/com/example/mad_practical7_20012011006/MyReceiver.kt
@@ -0,0 +1,23 @@
+package com.example.mad_practical7_20012011006
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val str1=intent.getStringExtra("Service1")
+        if(str1=="start" || str1=="Stop"){
+            val intentService= Intent(context,AlarmService::class.java)
+            intentService.putExtra("Service1",intent.getStringExtra("Service1"))
+            if(str1=="start"){
+                context.startService(intentService)
+            }
+            else if(str1=="Stop")
+            {
+                context.startService(intentService)
+            }
+        }
+    }
+}
